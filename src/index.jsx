aeabe53071c71e3287fa9aa4c2d80a1a403e2185
Josue Angel Perez Polanco <josue224yosi@gmail.com>
2026-06-05 00:18:41 -0600

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+    { path: "/", element: <App/>}
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
