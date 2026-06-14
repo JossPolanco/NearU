8aa028aef6ed93bae82038940950de6fdbe4dcbc
Josue Polanco <josue224yosi@gmail.com>
2026-06-13 19:20:03 -0600

    -Add empty page of configuration.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
-export {default as TestingPage} from "./TestingPage";
-export {default as DrawingPage} from "./DrawingPage";
-export {default as Home} from "./Home";
-export {default as Login} from "./Login";
-export {default as Register} from "./Register";
-export {default as NotFound} from "./NotFound";
-export {default as PasswordRegistration} from "./PasswordRegistration";
+export { default as PasswordRegistration } from "./PasswordRegistration";
+export { default as Configuration } from "./Configuration"
+export { default as TestingPage } from "./TestingPage";
+export { default as DrawingPage } from "./DrawingPage";
+export { default as Register } from "./Register";
+export { default as NotFound } from "./NotFound";
+export { default as Login } from "./Login";
+export { default as Home } from "./Home";
