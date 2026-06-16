28c6341e2cfb345e240e06e3cd6fd1b224bd557f
Josue Polanco <josue224yosi@gmail.com>
2026-06-16 17:59:49 -0600

    Add function to send message.

--- src/components/MessageField.jsx
+++ src/components/MessageField.jsx
@@ -1,15 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Send } from 'lucide-react';
+import { useState } from "react";
+import { sendMessage } from "../services/chat/messagesService";
 
 export default function MessageField() {
+    const [message, setMessage] = useState("");
+    const queryClient = useQueryClient();
+
+    const sendMessageMutation = useMutation({
+        mutationFn: sendMessage,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['messages'] });
+        },
+
+        onError: (error) => {
+            console.error("Error sending message:", error);
+        }
+    });
+
     return (
         <div className="sticky bottom-0 bg-base-300 px-4 py-2 flex gap-2">
             <input
                 type="text"
                 placeholder="Mensaje..."
                 className="input input-bordered flex-1"
+                onChange={(e) => setMessage(e.target.value)}
             />
 
-            <button className="btn btn-circle btn-primary">
+            <button className="btn btn-circle btn-primary" onClick={() => {sendMessageMutation.mutate({ contain: message })}}>
                 <Send size={20} />
             </button>
         </div>
